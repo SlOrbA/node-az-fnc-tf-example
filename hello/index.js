a862cf5f309e71b837dc823b5316c90f975ca302
Lari Korpi <lari.korpi@iki.fi>
2019-08-09 11:56:10 +0300

    remove extra semicolon

--- hello/index.js
+++ hello/index.js
@@ -1,6 +1,6 @@
 module.exports = function (context, req) {
 	context.log('hello call triggered with ' + req.method + ' method');
 	context.res =  {
-		body: "Hello World!";
+		body: "Hello World!"
 	};
 }
