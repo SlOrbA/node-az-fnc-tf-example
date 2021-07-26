985e97e05d9fc37dba281e40dc60955efabc2a8a
Lari Korpi <lari.korpi@iki.fi>
2021-07-26 11:17:54 +0300

    reformat with terraform fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,16 +11,16 @@ resource "azurerm_resource_group" "example" {
 
 resource "azurerm_storage_account" "example" {
   name                     = "nodeazfnctf${random_string.example.result}"
-  location                 = "${azurerm_resource_group.example.location}"
-  resource_group_name      = "${azurerm_resource_group.example.name}"
+  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.example.name
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_app_service_plan" "example" {
   name                = "node-az-fnc-tf-${random_string.example.result}-example-plan"
-  location            = "${azurerm_resource_group.example.location}"
-  resource_group_name = "${azurerm_resource_group.example.name}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   kind                = "FunctionApp"
 
   sku {
@@ -30,11 +30,11 @@ resource "azurerm_app_service_plan" "example" {
 }
 
 resource "azurerm_function_app" "example" {
-  name                       = "node-az-fnc-tf-${random_string.example.result}-example"
-  location                   = "${azurerm_resource_group.example.location}"
-  resource_group_name        = "${azurerm_resource_group.example.name}"
-  app_service_plan_id        = "${azurerm_app_service_plan.example.id}"
-  storage_connection_string  = "${azurerm_storage_account.example.primary_connection_string}"
+  name                      = "node-az-fnc-tf-${random_string.example.result}-example"
+  location                  = azurerm_resource_group.example.location
+  resource_group_name       = azurerm_resource_group.example.name
+  app_service_plan_id       = azurerm_app_service_plan.example.id
+  storage_connection_string = azurerm_storage_account.example.primary_connection_string
 
   app_settings = {
     APPINSIGHTS_INSTRUMENTATIONKEY = "${azurerm_application_insights.example.instrumentation_key}"
@@ -51,13 +51,13 @@ resource "azurerm_function_app" "example" {
     working_dir = ".."
   }
 
-  
+
 }
 
 resource "azurerm_application_insights" "example" {
   name                = "node-az-fnc-tf-${random_string.example.result}-example"
-  location            = "${azurerm_resource_group.example.location}"
-  resource_group_name = "${azurerm_resource_group.example.name}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   application_type    = "web"
 }
 
